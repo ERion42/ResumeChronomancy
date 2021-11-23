d989fe03e856f754bdeb61eeb3e89f8f45fbc72c
Lincoln Fewell <LincolnTFewell@gmail.com>
2021-11-23 13:56:12 -0500

    Created Experience schema

---
+++ server/models/Experience.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const experienceSchema = new Schema({
+    organization: {
+        type: String,
+        required: true,
+    },
+    position: {
+        type: String,
+        required: true,
+    },
+    start_date: {
+        type: Date,
+        required: true,
+        max: Date.now
+    },
+    end_date: {
+        type: Date,
+        required: false,
+        max: Data.now
+    },
+    location: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: false,
+    }
+});
+
+const Experience = model("Experience", experienceSchema);
+
+module.exports = Experience;
