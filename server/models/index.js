b334adc2d2a8a383b6eb1c7e73d5f8d15442667b
Matt Gomez <soraticat@gmail.com>
2021-11-23 12:53:28 -0500

    models index.js

---
+++ server/models/index.js
@@ -0,0 +1,11 @@
+const Profile = require('./Profile');
+const Experience = require('./Experience');
+const Education = require('./Education');
+const Skills = require('./Skills');
+
+module.exports = {
+	Profile,
+	Experience,
+	Education,
+	Skills
+}
