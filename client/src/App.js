1f376eacd31bc5969c975c784c2531f86f7e5de3
Eric Rion <erion42@gmail.com>
2021-11-23 23:27:50 -0500

    Corrected folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -34,17 +34,8 @@ function App() {
     <ApolloProvider client={client}>
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Resume Chronomancer</h1>
+          <p>Coming Soon</p>
         </header>
       </div>
     </ApolloProvider>
