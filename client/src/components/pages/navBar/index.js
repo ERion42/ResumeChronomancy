807e07fa9c77786b3a5fe7a346e70400e3f3c76b
Eric Rion <erion42@gmail.com>
2021-11-24 09:30:14 -0500

    Added basic React code for page script files

---
+++ client/src/components/pages/navBar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NavBar() {
+    return (
+        <p></p>
+    );
+}
+
+export default NavBar;
---
+++ client/src/components/pages/sideBar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SideBar() {
+    return (
+        <p></p>
+    );
+}
+
+export default SideBar;
