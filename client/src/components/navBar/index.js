8202bfe86527a69ed14fc4e8aa43abd6f6c1d387
Eric Rion <erion42@gmail.com>
2021-11-24 11:08:12 -0500

    NavBar exists- needs linking

--- client/src/components/navBar/index.js
+++ client/src/components/navBar/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import Logo from '../img/logo.png'
+
 
 function NavBar() {
     return (
-        <p></p>
+        <header class="p-3 bg-dark text-white">
+            <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                {/* Logo needs sizing work, too big right now */}
+                {/* <img src= {Logo}></img> */}
+                </a>
+
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Dashboard</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">FAQS</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+                </ul>
+
+                <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+                
+                </form>
+
+                <div class="text-end">
+                <button type="button" class="btn btn-outline-light me-2">Login</button>
+                <button type="button" class="btn btn-warning">Sign-up</button>
+                </div>
+            </div>
+            </div>
+        </header>        
     );
 }
 
