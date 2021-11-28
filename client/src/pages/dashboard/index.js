3c7ed42c2b0efd83b7f97a20987789b268568441
Eric Rion <erion42@gmail.com>
2021-11-28 10:55:08 -0500

    Added Google Font

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
-import BGImage from '../../components/img/geometricBackground.jpg';
-import LandingPage from '../landingPage';
 
 function Dashboard() {
     // nothing yet
     return (
         <div>
             <div className="bg-primary bg-gradient text-white">
-                <h1 className="p-5">Desktop</h1>
+                <h1 className="p-5">Dashboard</h1>
             </div>
             
             {/* Additional components can be placed here as needed */}
