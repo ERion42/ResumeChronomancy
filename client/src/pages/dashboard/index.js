0361d49217fa8d7887bcd61ae424ccf15e675924
Eric Rion <erion42@gmail.com>
2021-11-28 16:14:21 -0500

    dashboard set up to take forms when they're ready

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -10,14 +10,14 @@ function Dashboard() {
     return (
         <div className="container">
             <div class="row">
-                <div className="col-4">
+                <div className="col-4 bg-info rounded m-1">
                     <h2>Education</h2>
                     <EducationForm />
                 </div>
-                <div className="col-4">
+                <div className="col-md bg-warning rounded m-1">
                     <h2>Skills</h2>
                 </div>
-                <div className="col-4">
+                <div className="col-md bg-primary rounded m-1">
                     <h3>Experience</h3>
                 </div>
             </div>
