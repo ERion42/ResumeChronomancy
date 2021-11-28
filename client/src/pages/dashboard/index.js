e5dcbaf2d97fa0acfc9b85ec6520b3ccb60fc78d
Eric Rion <erion42@gmail.com>
2021-11-28 00:00:48 -0500

    One last commit before beddy-bye

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Navbar } from 'react-bootstrap';
+import BGImage from '../../components/img/geometricBackground.jpg';
+import LandingPage from '../landingPage';
 
 function Dashboard() {
     // nothing yet
     return (
-        <p>Dashboard</p>
+        <div>
+            <div className="bg-primary bg-gradient text-white">
+                <h1 className="p-5">Desktop</h1>
+            </div>
+            
+            {/* Additional components can be placed here as needed */}
+        </div>
     );
 }
 
