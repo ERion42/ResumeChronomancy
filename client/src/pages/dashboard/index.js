f7a6ba4b388abe2f943b288366f94ba1029a49c4
Eric Rion <erion42@gmail.com>
2021-11-28 20:25:38 -0500

    A few minor front-end changes

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -4,11 +4,11 @@ import EducationForm from '../../components/forms/EducationForm';
 
 
 function Dashboard() {
-    // nothing yet
+    // Waiting for forms for Skills and Experience... odds are we're going to have to re-do this one. I feel like we probably need 
     return (
         <div className="container ">
             <div class="row">
-                <div className="col-4 bg-info rounded m-1 pb-2">
+                <div className="col-md-4 bg-info rounded m-1 pb-2">
                     <h2>Education</h2>
                     <EducationForm />
                 </div>
