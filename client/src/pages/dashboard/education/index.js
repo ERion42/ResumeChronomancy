4874922e2bb59b0365da53342070c3dfdc227531
Eric Rion <erion42@gmail.com>
2021-11-28 21:11:55 -0500

    Education Dashboard p much done, visually

--- client/src/pages/dashboard/education/index.js
+++ client/src/pages/dashboard/education/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import EducationForm from '../../components/forms/EducationForm';
+import EducationForm from '../../../components/forms/EducationForm';
 
 
 
@@ -7,17 +7,32 @@ function DBEducation() {
     // nothing yet
     return (
         <div className="container">
-            <div class="row-4">
-                <div className="col-4 bg-info rounded m-1 pb-2">
+            <div className="row justify-content-center">
+                <div className="col-md-5 bg-warning rounded m-1 pb-2">
                     <h2>Update Education</h2>
                     <EducationForm />
                 </div>
-                <div className="col-md bg-warning rounded m-1">
+                <div className="col-md-6 bg-info rounded m-1">
                     <h2>My Education</h2>
-                    
+                    <ul className="list-group">
+                        <li className="list-group-item mt-4 active">Item</li>
+                        <li className="list-group-item">Item</li>
+                        <li className="list-group-item">Item</li>
+                        <li className="list-group-item">Item</li>
+                        <li className="list-group-item">Item</li>
+                        <li className="list-group-item">Item</li>
+                        <li className="list-group-item">Item</li>
+                        <li className="list-group-item">Item</li>
+                        <li className="list-group-item">Item</li>
+                        <li className="list-group-item">Item</li>
+                        <li className="list-group-item">Item</li>
+                        <li className="list-group-item">Item</li>
+                        
+                    </ul>
                 </div>
                 
             </div>
+
             <div className="row justify-content-center">
                 <div className="col-6 bg-success rounded m-1 text-center text-white">
                     <h1>Generate Resume</h1>
