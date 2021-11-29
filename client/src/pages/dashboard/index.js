045cce69b037b3875bb838618de7904e5b46f100
Eric Rion <erion42@gmail.com>
2021-11-28 19:51:02 -0500

    Added Google Font, mock-button for Generate Resume, and mock-forms for Skills and Experience on Dashboard

--- client/src/pages/dashboard/index.js
+++ client/src/pages/dashboard/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import EducationForm from '../../components/forms/EducationForm';
-// import ExperienceForm from '../../components/forms/ExperiencesForm';
-// import SkillForm from '../../components/forms/SkillsForm';
 
 
 
@@ -16,9 +14,16 @@ function Dashboard() {
                 </div>
                 <div className="col-md bg-warning rounded m-1">
                     <h2>Skills</h2>
+                    <EducationForm />
                 </div>
                 <div className="col-md bg-primary rounded m-1">
                     <h3>Experience</h3>
+                    <EducationForm />
+                </div>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col-6 bg-success rounded m-1 text-center text-white">
+                    <h1>Generate Resume</h1>
                 </div>
             </div>
         </div>
