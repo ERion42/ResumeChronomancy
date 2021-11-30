649c5eb2aa0801808956a425f5c1d81a8b87b4d3
Eric Rion <erion42@gmail.com>
2021-11-30 13:12:49 -0500

    made margins uniform for all dashboard pages

--- client/src/pages/dashboard/experience/index.js
+++ client/src/pages/dashboard/experience/index.js
@@ -8,15 +8,15 @@ function DBExperience() {
     return (
         <div className="container">
             <div className="row justify-content-center">
-                <div className="col-md-5 bg-warning rounded m-1 pb-2">
+                <div className="col-md-5 bg-warning rounded m-1 pb-2 mt-4">
                     <h2>Update Experience</h2>
                     <ExperienceForm />
                 </div>
-                <div className="col-md-6 bg-info rounded m-1">
+                <div className="col-md-6 bg-info rounded m-1 mt-4">
                     <h2>My Experience</h2>
                     <ul className="list-group">
-                                                {/* Template for Item List */}
-                                                <div className="list-group-item list-group-item-action">
+                        {/* Template for Item List */}
+                        <div className="list-group-item list-group-item-action">
                             <div className="d-flex w-100 justify-content-between">
                                 <h5 className="mb-1">Item Heading</h5>
                             </div>
